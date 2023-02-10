c288daf5f8b1849572f0a3f4cb74fe5080d3681c
javad <javad.feyzabadi@yahoo.com>
2023-02-10 22:03:33 +0330

    registerview

--- account/views.py
+++ account/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 from .forms import UserRegistrationForm
 
+
 class Registerview(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
 
     def get(self,request):
-        form = UserRegistrationForm()
-        return render(request,'account/register.html',{'form':form})
+        form = self.form_class()
+        return render(request,self.template_name,{'form':form})
+
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'],cd['email'],cd['password'])
+            messages.success(request,'your registred successfully','success')
+            return redirect('home:home')    
+        return render(request,self.template_name,{'form':form})
