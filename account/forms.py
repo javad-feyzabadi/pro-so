143d31aae6d6353740e0d555a4f015dcb8d20d96
javad <javad.feyzabadi@yahoo.com>
2023-02-10 22:23:18 +0330

    clean_

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email = email).exists()
+        if user:
+            raise ValidationError("This Email Already Exists!")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username = username).exists()
+        if user :
+            raise ValidationError("This UserName Already Exists!")
+        return username
+
