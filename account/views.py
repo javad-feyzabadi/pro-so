2fbc645959b4e14b9156f535bf56dcea9d833eb0
javad <javad.feyzabadi@yahoo.com>
2023-02-11 01:49:23 +0330

    add method dispatch

--- account/views.py
+++ account/views.py
@@ -11,6 +11,11 @@ class UserRegisterview(View):
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
 
+    def dispatch(self,request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self,request):
         form = self.form_class()
         return render(request,self.template_name,{'form':form})
@@ -28,6 +33,11 @@ class UserRegisterview(View):
 class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
+
+    def dispatch(self,request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self,request):
         form = self.form_class()
