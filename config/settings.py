a5501ff3925db46ac1d857f050d8143adbef5935
javad <javad.feyzabadi@yahoo.com>
2023-02-11 10:41:03 +0330

    authentications backends

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/account/login/'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authenticate.EmailBackend',
+]
