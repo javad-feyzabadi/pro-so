a67d27df0b8835e5c9121bf666cbe6b472927871
javad <javad.feyzabadi@yahoo.com>
2023-02-12 01:58:45 +0330

    delete post

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from . views import HomeView,PostDetailView
+from . views import HomeView,PostDetailView,PostDeleteView
 
 app_name = 'home'
 
 urlpatterns = [
     path('',HomeView.as_view(),name ='home'),
-    path('post/<int:post_id>/<slug:post_slug>/',PostDetailView.as_view(),name='post_detail')
+    path('post/<int:post_id>/<slug:post_slug>/',PostDetailView.as_view(),name='post_detail'),
+    path('post/delete/<int:post_id>//',PostDeleteView.as_view(),name='post_delete'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 from . models import Post
 
 class HomeView(View):
@@ -10,6 +14,21 @@ class HomeView(View):
 
 
 class PostDetailView(View):
+
     def get(self,request,post_id,post_slug):
         post = Post.objects.get(pk = post_id,slug = post_slug)
         return render(request,'home/detail.html',{'post':post})
+
+
+class PostDeleteView(LoginRequiredMixin,View):
+    
+    def get(self,request,post_id):
+        post = Post.objects.get(pk = post_id)
+        if post.user.id == request.user.id :
+            post.delete()
+            messages.success(request,"This Post Deleted",'success')
+        else:
+            messages.error(request,'You cant Delete This Post','danger')
+        return redirect('home:home')
+
+         
