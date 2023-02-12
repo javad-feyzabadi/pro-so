a474e60af0c5b6405f215217a4f5b178514c0b72
javad <javad.feyzabadi@yahoo.com>
2023-02-13 00:17:32 +0330

    get_object_or_404

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!iv@w7d-ewpy)-&%%&549mv-^l2hvdpjs9mmp9hzv3d8-r2p*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
