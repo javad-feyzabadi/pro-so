c5b4f3caa20959334209629bf0d29b0011fcded3
javad <javad.feyzabadi@yahoo.com>
2023-02-13 00:04:31 +0330

    create post

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . views import HomeView,PostDetailView,PostDeleteView,PostUpdateView
+from . views import HomeView,PostDetailView,PostDeleteView,PostUpdateView,PostCreateView
 
 app_name = 'home'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('post/<int:post_id>/<slug:post_slug>/',PostDetailView.as_view(),name='post_detail'),
     path('post/delete/<int:post_id>/',PostDeleteView.as_view(),name='post_delete'),
     path('post/update/<int:post_id>/',PostUpdateView.as_view(),name='post_update'),
+    path('post/create/',PostCreateView.as_view(),name='post_create'),
 
 
 ]
--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.utils.text import slugify
 from . models import Post
-from . forms import PostUpdateForm
+from . forms import PostCreateUpdateForm
 
 class HomeView(View):
     
@@ -34,7 +34,7 @@ class PostDeleteView(LoginRequiredMixin,View):
 
 
 class PostUpdateView(LoginRequiredMixin,View):
-    form_class = PostUpdateForm
+    form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
         self.post_instance = Post.objects.get(pk = kwargs['post_id'])
@@ -62,5 +62,22 @@ class PostUpdateView(LoginRequiredMixin,View):
             messages.success(request,'You Updated This Post','success')
             return redirect('home:post_detail',post.id , post.slug)
 
+
+class PostCreateView(LoginRequiredMixin,View):
+    form_class = PostCreateUpdateForm
+
+    def get(self,request,*args,**kwargs):
+        form = self.form_class
+        return render(request,'home/create.html',{'form':form})
+    
+    def post(self,request,*args,**kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request,'You Created The New Post','success')
+            return redirect('home:post_detail',new_post.id , new_post.slug)
 
          
